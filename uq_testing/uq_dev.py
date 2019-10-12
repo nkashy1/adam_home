a6564c2944fddf713190d2a8c4dd1c09458f25dd
vvittaldev <vivekvittaldev@gmail.com>
2019-10-11 17:02:51 -0700

    UQ: sigma point uncertainty computation

---
+++ uq_testing/uq_dev.py
@@ -0,0 +1,119 @@
+import numpy as np
+from scipy.linalg import cholesky
+
+
+def sigma_pts_ddt(x, Pxx, h=np.sqrt(3.0)):
+    """Get sigma points for the Divided Difference Transform
+
+    DDT is a method for propagating uncertainty of an
+    initial Gaussian distribution
+
+    X~N(x, Pxx)
+
+    args:
+        x: numpy array of initial state mean
+        Pxx: numpy matrix of initial covariance
+        h = step size used in DDT (default = sqrt(3))
+
+    out:
+        sig_pts: list of numpy arrays
+            sigma points are in format (2D case):
+                sig_pts[0] = x + h * Sxx[:, 0]
+                sig_pts[1] = x - h * Sxx[:, 0]
+                sig_pts[2] = x + h * Sxx[:, 1]
+                sig_pts[3] = x - h * Sxx[:, 1]
+                sig_pts[4] = x
+
+    """
+    # Lower triangular matrix such that P = Sxx * transpose(Sxx)
+    Sxx = cholesky(Pxx, lower=True)
+    sig_pts = []
+    for col in Sxx.T:  # loop over columns
+        sig_pts.append(x + (h * col))
+        sig_pts.append(x - (h * col))
+    sig_pts.append(x)
+    return sig_pts
+
+
+def mean_cov_ddt2(sig_pts, h=np.sqrt(3.0)):
+    """Compute mean and covariance from sigma point of the
+    second order Divided Difference Transform
+
+    Y = f(X) where f is any nonlinear function acting
+    on a multivariate gaussian initial condition
+    X~N(x, Pxx)
+    Estimate Y~N(y, Pyy)
+
+    This function computes mean and covariance of y
+    using sigma points of x propagated through f
+
+    args:
+        sig_pts: list of numpy arrays of sigma points
+            For a 2d x:
+            sig_pts[0] = f(x + h * Sxx[:, 0])
+            sig_pts[1] = f(x - h * Sxx[:, 0])
+            sig_pts[2] = f(x + h * Sxx[:, 1])
+            sig_pts[3] = f(x - h * Sxx[:, 1])
+            sig_pts[4] = f(x)
+        h = step size used in DDT (default = sqrt(3))
+    out:
+        y: estimate of mean of Y
+        Pyy: estimate of covariance of Y
+
+    """
+    x_vars_count = int((len(sig_pts) - 1) / 2)
+    y_vars_count = len(sig_pts[0])
+    y_x = sig_pts[-1]  # f(x)
+    # Covariance related
+    Syy_ord_1 = np.zeros([y_vars_count, x_vars_count])
+    Syy_ord_2 = np.zeros_like(Syy_ord_1)
+    # TODO Easy to read but not computationally efficient or elegent
+    # Mean related
+    y_mean = ((h ** 2 - x_vars_count) / (h ** 2)) * y_x
+    for var in range(0, x_vars_count):
+        y_xpdx = sig_pts[2 * var]  # f(x+ dx)
+        y_xmdx = sig_pts[2 * var + 1]  # f(x - dx)
+        # Mean related
+        y_mean += (y_xpdx + y_xmdx) / (2.0 * h ** 2)
+        # Covariance related
+        Syy_ord_1[:, var] = (y_xpdx - y_xmdx) / (2.0 * h)
+        Syy_ord_2[:, var] = (y_xpdx + y_xmdx - (2.0 * y_x)) * (
+            np.sqrt(h ** 2 - 1.0) / (2.0 * h ** 2)
+        )
+    # [Syy_ord_1, Syy_ord_2]
+    Syy_rect = np.concatenate((Syy_ord_1, Syy_ord_2), axis=1)
+    # TODO compute Sxx using Householder transformation
+    # We just want the covariance matrix, so no need to find square-root
+    Pyy = np.dot(Syy_rect, Syy_rect.T)
+    return y_mean, Pyy
+
+
+def propagate_mean_cov_ddt(f_x, x, P_xx, h=np.sqrt(3)):
+    """Compute mean and covariance using the
+    second order Divided Difference Transform
+
+    Y = f(X) where f is any nonlinear function acting
+    on a multivariate gaussian initial condition
+    X~N(x, Pxx)
+    Estimate Y~N(y, Pyy)
+
+    This function computes mean and covariance of y
+    using sigma points of x propagated through f
+
+    N.B. f_x is a function that takes vectorial numpy array inputs
+    i.e. Y = [[y0], [y1], ..., [yN]] (2D np array where each aray is a sample)
+         X = [[x0], [x1], ..., [xN]] (2D np array where each aray is a sample)
+         Y = f_x(X)
+
+    args:
+        f_x: function input
+        h = step size used in DDT (default = sqrt(3))
+    out:
+        y: estimate of mean of Y
+        Pyy: estimate of covariance of Y
+
+    """
+    x_sigma_pts = sigma_pts_ddt(x, P_xx, h)
+    y_sigma_pts = f_x(x_sigma_pts)
+    y, P_yy = mean_cov_ddt2(y_sigma_pts, h)
+    return (y, P_yy)
